bea340822f4b6a8c2fea2d4d3a8cd4d840f6ee54
whitelord <1091285927@qq.com>
2019-06-02 11:43:33 +0800

    Add files via upload

---
+++ ex01/ex01.py
@@ -0,0 +1,222 @@
+
+#%%
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+path =  'ex01\ex1data1.txt'
+data = pd.read_csv(path, header=None, names=['Population', 'Profit'])
+data.head()
+
+
+#%%
+data.describe()
+
+#%% [markdown]
+# 看下数据长什么样子
+
+#%%
+data.plot(kind='scatter', x='Population', y='Profit', figsize=(12,8))
+plt.show()
+
+#%% [markdown]
+# 现在让我们使用梯度下降来实现线性回归，以最小化成本函数。 
+#%% [markdown]
+# <font color=red size=3> #############请在下面写出线性回归的代价函数（备注：函数名字为computeCost(X, y, theta)）#################### </font>
+def computeCost(X, y, theta):
+    print(X)
+    print(theta)
+    tem = np.dot(X, theta.T)
+    print(tem.shape)
+    temp = tem - y
+    print(temp)
+    print(temp.shape)
+    result = [[temp[i][j]**2 for j in range(len(temp[i]))] for i in range(len(temp))]
+    print(result)
+    return np.sum(result)/(2*len(temp))
+
+#%% [markdown]
+# 让我们在训练集中添加一列，以便我们可以使用向量化的解决方案来计算代价和梯度。
+
+#%%
+data.insert(0, 'Ones', 1)
+# 在第0列 'population'前加入一列名为'Ones' 值为全1的列
+
+#%% [markdown]
+# 现在我们来做一些变量初始化。
+# 将源数据表格拆分为输入x和真实值y
+cols = data.shape[1]
+#X是所有行，去掉最后一列
+X = data.iloc[:,0:cols-1]
+#X是所有行，最后一列
+y = data.iloc[:,cols-1:cols]
+
+#%% [markdown]
+# 观察下 X (训练集) and y (目标变量)是否正确.
+
+#%%
+X.head()#head()是观察前5行
+
+
+#%%
+y.head()
+
+#%% [markdown]
+# 代价函数是应该是numpy矩阵，所以我们需要转换X和Y，然后才能使用它们。 我们还需要初始化theta。
+
+#%%
+#x为两列的矩阵，y为一列
+X = np.matrix(X.values)
+y = np.matrix(y.values)
+theta = np.matrix(np.array([0,0]))
+
+#%% [markdown]
+# theta 是一个(1,2)矩阵
+
+#%%
+y
+
+
+#%% [markdown]
+# 看下维度
+
+#%%
+X.shape, theta.shape, y.shape
+
+#%% [markdown]
+# 计算代价函数 (theta初始值为0).
+
+#%%
+
+# 初始化 theta为(0,0)
+computeCost(X, y, theta)
+
+#%% [markdown]
+# # batch gradient decent（批量梯度下降）
+# $${{\theta }_{j}}:={{\theta }_{j}}-\alpha \frac{\partial }{\partial {{\theta }_{j}}}J\left( \theta  \right)$$
+#%% [markdown]
+# <font color=red size=3> #############请在下面写出线性回归的批量梯度下降代码（备注：函数名字为gradientDescent(X, y, theta, alpha, iters) 要求返回模型参数theta 以及 每次迭代过程中的代价函数 cost）#################### </font>
+
+#%%
+#写出线性回归的梯度下降函数
+def gradientDescent(X, y, theta, alpha, iters):
+    temp = np.matrix(np.zeros(theta.shape))
+    parameters = int(theta.ravel().shape[1])
+    cost = np.zeros(iters)
+    
+    for i in range(iters):
+        # theta0 公式
+        part_1 = np.dot(X, theta.T) - y
+        
+        for j in range(parameters):
+            # theta1 公式
+            part_2 = np.multiply(part_1, X[:, j])
+            temp[0, j] -= alpha * np.sum(part_2)/len(X)
+        theta = temp
+        cost[i] = computeCost(X, y, theta)
+    return theta, cost
+
+#%% [markdown]
+# 初始化一些附加变量 - 学习速率α和要执行的迭代次数。
+# 注意：在此处更改学习率，看看对模型的训练有什么影响
+
+#%%
+alpha = 0.001
+iters = 1000
+
+#%% [markdown]
+# 现在让我们运行梯度下降算法来将我们的参数θ适合于训练集。
+
+#%%
+g, cost = gradientDescent(X, y, theta, alpha, iters)
+
+g
+
+#%% [markdown]
+# 最后，我们可以使用我们拟合的参数计算训练模型的代价函数（误差）。
+
+#%%
+computeCost(X, y, g)
+
+#%% [markdown]
+# 现在我们来绘制线性模型以及数据，直观地看出它的拟合。
+
+# #%%
+# # 设置x取值
+# x = np.linspace(data.Population.min(), data.Population.max(), 100)
+# # 设置y取值 ： J(theta) 公式
+# f = g[0, 0] + (g[0, 1] * x)
+
+# fig, ax = plt.subplots(figsize=(12,8))
+# ax.plot(x, f, 'r', label='Prediction')
+# ax.scatter(data.Population, data.Profit, label='Traning Data')
+# ax.legend(loc=2)
+# ax.set_xlabel('Population')
+# ax.set_ylabel('Profit')
+# ax.set_title('Predicted Profit vs. Population Size')
+# plt.show()
+
+# #%% [markdown]
+# # 由于梯度方程式函数也在每个训练迭代中输出一个代价的向量，所以我们也可以绘制。 请注意，代价总是降低 - 这是凸优化问题的一个例子。
+
+# #%%
+# fig, ax = plt.subplots(figsize=(12,8))
+
+# #横轴：迭代次数 ；纵轴： 误差
+# ax.plot(np.arange(iters), cost, 'r')
+# ax.set_xlabel('Iterations')
+# ax.set_ylabel('Cost')
+# ax.set_title('Error vs. Training Epoch')
+# plt.show()
+
+
+
+
+#%%
+
+# 改变学习速率生成比较图的函数 draw_compare(X,y,theta,alpha1,alpha2,iters)
+alpha1 = 0.001
+alpha2 = 0.01
+iters = 1000
+
+def draw_prepare(X,y,theta,alpha,iters):
+    g, cost = gradientDescent(X, y, theta, alpha, iters)
+    computeCost(X, y, g)
+    # 设置x取值
+    x = np.linspace(data.Population.min(), data.Population.max(), 100)
+    # 设置y取值 ： J(theta) 公式
+    f = g[0, 0] + (g[0, 1] * x)
+    return g,cost,x,f
+    
+
+def draw_predicted(x1,f1,alpha1,x2,f2,alpha2):
+    fig, ax = plt.subplots(figsize = (12,8))
+    ax.plot(x1, f1, 'r', label='Prediction while alpha = '+ str(alpha1))
+    ax.plot(x2, f2, 'purple', label='Prediction while alpha = '+ str(alpha2))
+    ax.scatter(data.Population, data.Profit, label='Traning Data')
+    ax.legend(loc=2)
+    ax.set_xlabel('Population')
+    ax.set_ylabel('Profit')
+    ax.set_title('Predicted Profit vs. Population Size')
+    plt.show()
+
+def draw_cost(cost1,cost2,iters):
+    fig, ax = plt.subplots(figsize = (12,8))
+    #横轴：迭代次数 ；纵轴： 误差
+    ax.plot(np.arange(iters), cost1, 'r',label = 'alpha1 cost')
+    ax.plot(np.arange(iters), cost2, 'blue',label = 'alpha2 cost')
+    ax.legend(loc=1)
+    ax.set_xlabel('Iterations')
+    ax.set_ylabel('Cost')
+    ax.set_title('Error vs. Training Epoch')
+    plt.show()
+
+def draw_compare(X,y,theta,alpha1,alpha2,iters):
+    g1,cost1,x1,f1 = draw_prepare(X,y,theta,alpha1,iters)
+    g2,cost2,x2,f2 = draw_prepare(X,y,theta,alpha2,iters)
+    draw_predicted(x1,f1,alpha1,x2,f2,alpha2)
+    draw_cost(cost1,cost2,iters)
+
+draw_compare(X,y,theta,alpha1,alpha2,iters)
+
+
+#%%
---
+++ ex02/ex02.py
@@ -0,0 +1,192 @@
+#%% [markdown]
+# # 机器学习练习 2 - 逻辑回归
+#%% [markdown]
+# ## 逻辑回归
+#%% [markdown]
+# 在训练的初始阶段，我们将要构建一个逻辑回归模型来预测，某个学生是否被大学录取。设想你是大学相关部分的管理者，想通过申请学生两次测试的评分，来决定他们是否被录取。现在你拥有之前申请学生的可以用于训练逻辑回归的训练样本集。对于每一个训练样本，你有他们两次测试的评分和最后是被录取的结果。为了完成这个预测任务，我们准备构建一个可以基于两次测试评分来评估录取可能性的分类模型。
+
+#%%
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+#%%
+path = 'ex02\ex2data1.txt'
+data = pd.read_csv(path, header=None, names=['Exam 1', 'Exam 2', 'Admitted'])
+data.head()
+
+#%% [markdown]
+# 让我们创建两个分数的散点图，并使用颜色编码来可视化，如果样本是正的（被接纳）或负的（未被接纳）。
+
+#%%
+positive = data[data['Admitted'].isin([1])]
+negative = data[data['Admitted'].isin([0])]
+
+fig, ax = plt.subplots(figsize=(12,8))
+ax.scatter(positive['Exam 1'], positive['Exam 2'], s=50, c='b', marker='o', label='Admitted')
+ax.scatter(negative['Exam 1'], negative['Exam 2'], s=50, c='r', marker='x', label='Not Admitted')
+ax.legend()
+ax.set_xlabel('Exam 1 Score')
+ax.set_ylabel('Exam 2 Score')
+plt.show()
+
+#%% [markdown]
+# 看起来在两类间，有一个清晰的决策边界。现在我们需要实现逻辑回归，那样就可以训练一个模型来预测结果。
+#%% [markdown]
+# # sigmoid 函数
+# g 代表一个常用的逻辑函数（logistic function）为S形函数（Sigmoid function），公式为： \\[g\left( z \right)=\frac{1}{1+{{e}^{-z}}}\\] 
+# 合起来，我们得到逻辑回归模型的假设函数： 
+# 	\\[{{h}_{\theta }}\left( x \right)=\frac{1}{1+{{e}^{-{{\theta }^{T}}X}}}\\] 
+#%% [markdown]
+# <font color=red size=3> #############请在下面写出sigmoid函数sigmoid(z)#################### </font>
+
+#%%
+def sigmoid(z):
+    return 1 / (1 + np.exp(-z))
+
+#%% [markdown]
+# 让我们做一个快速的检查，来确保它可以工作。
+
+#%%
+nums = np.arange(-10, 10, step=1)
+
+fig, ax = plt.subplots(figsize=(12,8))
+ax.plot(nums, sigmoid(nums), 'r')
+plt.show()
+
+#%% [markdown]
+# 棒极了！现在，我们需要编写代价函数来评估结果。
+# 代价函数：
+# $J\left( \theta  \right)=\frac{1}{m}\sum\limits_{i=1}^{m}{[-{{y}^{(i)}}\log \left( {{h}_{\theta }}\left( {{x}^{(i)}} \right) \right)-\left( 1-{{y}^{(i)}} \right)\log \left( 1-{{h}_{\theta }}\left( {{x}^{(i)}} \right) \right)]}$
+#%% [markdown]
+# 现在，我们要做一些设置，和我们在练习1在线性回归的练习很相似。
+
+#%%
+# add a ones column - this makes the matrix multiplication work out easier
+data.insert(0, 'Ones', 1)
+
+# set X (training data) and y (target variable)
+cols = data.shape[1]
+X = data.iloc[:,0:cols-1]
+y = data.iloc[:,cols-1:cols]
+
+# convert to numpy arrays and initalize the parameter array theta
+X = np.array(X.values)
+y = np.array(y.values)
+theta = np.zeros(3)
+
+#%% [markdown]
+# 让我们来检查矩阵的维度来确保一切良好。
+
+#%%
+theta
+
+
+#%%
+X.shape, theta.shape, y.shape
+
+#%% [markdown]
+# <font color=red size=3> #############请在下面写出逻辑斯蒂回归的代价函数cost(theta, X, y)，函数返回计算的代价函数值#################### </font>
+
+#%%
+def cost(theta, X, y):
+    
+    
+    
+    
+    
+
+#%% [markdown]
+# 让我们计算初始化参数的代价函数(theta为0)。
+
+#%%
+cost(theta, X, y)
+
+#%% [markdown]
+# 看起来不错，接下来，我们需要一个函数来计算我们的训练数据、标签和一些参数thata的梯度。
+#%% [markdown]
+# # gradient descent(梯度下降)
+# * 这是批量梯度下降（batch gradient descent）  
+# * 转化为向量化计算： $\frac{1}{m} X^T( Sigmoid(X\theta) - y )$
+# $$\frac{\partial J\left( \theta  \right)}{\partial {{\theta }_{j}}}=\frac{1}{m}\sum\limits_{i=1}^{m}{({{h}_{\theta }}\left( {{x}^{(i)}} \right)-{{y}^{(i)}})x_{_{j}}^{(i)}}$$
+#%% [markdown]
+# <font color=red size=3> #############请在下面写出逻辑斯蒂回归的梯度函数gradient(theta, X, y)，函数返回计算的梯度,注意这里不是写出梯度下降算法，而是要求写出求梯度的函数，目的是在下面使用scipy.optimize优化的时候使用#################### </font>
+
+#%%
+def gradient(theta, X, y):
+    
+    
+    
+    
+    
+    
+    
+    
+
+#%% [markdown]
+# 注意，我们实际上没有在这个函数中执行梯度下降，我们仅仅在计算一个梯度步长。我们可以用SciPy的“optimize”命名空间来做同样的事情。
+#%% [markdown]
+# 我们看看用我们的数据和初始参数为0的梯度下降法的结果。
+
+#%%
+gradient(theta, X, y)
+
+#%% [markdown]
+# 现在可以用SciPy's truncated newton（TNC）实现寻找最优参数。
+#%% [markdown]
+# 调用：
+# 
+# scipy.optimize.fmin_tnc(func, x0, fprime=None, args=(), approx_grad=0, bounds=None, epsilon=1e-08, scale=None, offset=None, messages=15, maxCGit=-1, maxfun=None, eta=-1, stepmx=0, accuracy=0, fmin=0, ftol=-1, xtol=-1, pgtol=-1, rescale=-1, disp=None, callback=None)
+# 
+# 最常使用的参数：
+# 
+# func：优化的目标函数
+# 
+# x0：初值
+# 
+# fprime：提供优化函数func的梯度函数，不然优化函数func必须返回函数值和梯度，或者设置approx_grad=True
+# 
+# approx_grad :如果设置为True，会给出近似梯度
+# 
+# args：元组，是传递给优化函数的参数
+
+#%%
+import scipy.optimize as opt
+result = opt.fmin_tnc(func=cost, x0=theta, fprime=gradient, args=(X, y))
+result
+
+#%% [markdown]
+# 让我们看看在这个结论下代价函数计算结果是什么个样子~
+
+#%%
+cost(result[0], X, y)
+
+#%% [markdown]
+# 接下来，我们需要编写一个函数，用我们所学的参数theta来为数据集X输出预测。然后，我们可以使用这个函数来给我们的分类器的训练精度打分。
+# 逻辑回归模型的假设函数： 
+# 	\\[{{h}_{\theta }}\left( x \right)=\frac{1}{1+{{e}^{-{{\theta }^{T}}X}}}\\] 
+# 当${{h}_{\theta }}$大于等于0.5时，预测 y=1
+# 
+# 当${{h}_{\theta }}$小于0.5时，预测 y=0 。
+
+#%%
+def predict(theta, X):
+    probability = sigmoid(X * theta.T)
+    return [1 if x >= 0.5 else 0 for x in probability]
+
+
+#%%
+theta_min = np.matrix(result[0])
+predictions = predict(theta_min, X)
+correct = [1 if ((a == 1 and b == 1) or (a == 0 and b == 0)) else 0 for (a, b) in zip(predictions, y)]
+accuracy = (sum(map(int, correct)) % len(correct))
+print ('accuracy = {0}%'.format(accuracy))
+
+#%% [markdown]
+# 我们的逻辑回归分类器预测正确，如果一个学生被录取或没有录取，达到89%的精确度。不坏！记住，这是训练集的准确性。我们没有保持住了设置或使用交叉验证得到的真实逼近，所以这个数字有可能高于其真实值（这个话题将在以后说明）。
+
+#%%
+
+
+
